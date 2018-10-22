d5fdfc91e687a582863d2a7617aa67fac612c166
phail <phel@hacknology.de>
2018-10-22 19:05:37 +0200

    motor communication in task

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <crc.h>
 #include "serialbridge.h"
 
+TaskHandle_t TaskMainloop, TaskMotorcommunication;
+void mainloop(void *pvParameters);
+void motorCommunication(void *pvParameters);
+
 #ifdef PADDELEC
   #include "Paddelec.h"
   Paddelec paddelec = Paddelec();
@@ -77,6 +81,24 @@ void setup() {
 
   /* Keep this at the end of setup */
   nextMillisMotorInput = millis();
+
+  xTaskCreatePinnedToCore(
+    mainloop,                 /* Task function. */
+    "Main_loop",              /* name of task. */
+    1000,                     /* Stack size of task */
+    (void *)1,                /* parameter of the task */
+    1,                        /* priority of the task */
+    &TaskMainloop,            /* Task handle to keep track of created task */
+    1);                       /* Core (0 is used by ESP32 connectivity) */
+
+  xTaskCreatePinnedToCore(
+    mainloop,                 /* Task function. */
+    "Motor_Comm",             /* name of task. */
+    1000,                     /* Stack size of task */
+    (void *)1,                /* parameter of the task */
+    1,                        /* priority of the task */
+    &TaskMotorcommunication,  /* Task handle to keep track of created task */
+    0);                       /* Core (0 is used by ESP32 connectivity) */
 }
 
 
@@ -105,84 +127,100 @@ void updateSpeed() {
 }
 
 void loop() {  
-#ifdef OTA_HANDLER  
-  ota();
-#endif // OTA_HANDLER
+  // nope, do nothing here
+  vTaskDelay(portMAX_DELAY); // wait as much as posible ...
+}
 
-#ifdef WIFI
-  bridge();
-#endif
+void mainloop( void *pvparameters ) {
+  int taskno = (int)pvparameters;
+  while(1) {
+  #ifdef OTA_HANDLER  
+    ota();
+  #endif // OTA_HANDLER
 
-  deltaMillis = millis() - nextMillisMotorInput;
-  if(deltaMillis >= 0) {
-    nextMillisMotorInput += MOTORINPUT_PERIOD;
-
-#ifdef PADDELEC
-    paddelec.update(motor.pwm, motor.steer, motor.actualSpeed_kmh, motor.actualSteer_kmh, deltaMillis);
-    if(debug) {
-      paddelec.debug(*COM[DEBUG_COM]);
-      for(byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
-        if(TCPClient[1][cln]) paddelec.debug(TCPClient[1][cln]); 
-    }
-#endif // PADDELEC
-
-#ifdef BLE
-//  loopBLE();
-motor.pwm = ble_pitch;
-motor.steer = ble_roll;
-slowReset(ble_pitch, 0.0, 0.1);
-slowReset(ble_roll, 0.0, 0.1);
-#endif
-
-#ifdef IMU
-//  imu.loopIMU();
-  imu.update(motor.pwm, motor.steer);
-  imu.debug(*COM[DEBUG_COM]);
-#endif
-
-#if defined(NUNCHUCK) && defined(PADDELEC)
-    if(paddelec.gametrak1.r < 500 || paddelec.gametrak2.r < 500) { // switch to nunchuck control when paddle is not used
-#endif
-#if defined(NUNCHUCK)
-      int nunchuckError = nunchuk.update(motor.pwm, motor.steer);
-      ++errorCount;
-      nunchuk.debug(*COM[DEBUG_COM]);
-      if(nunchuckError >= 1000) {
-        if(debug) COM[DEBUG_COM]->printf("Reinit Nunchuck %4i ", nunchuckError);
-      } else if(nunchuckError >= 100) {
-        if(debug) COM[DEBUG_COM]->printf("I2C Problems %4i ", nunchuckError);
-      } else if(nunchuckError > 0) {
-        if(debug) COM[DEBUG_COM]->printf("Nunchuck Comm Problems %4i ", nunchuckError);
-      } else  {
-        errorCount = 0;
-      }
-
-      if(errorCount>=5) {
-        nunchuk.reInit();
-        errorCount = 0;
-      }
-#endif
-#if defined(NUNCHUCK) && defined(PADDELEC)
-    }
-#endif
-
-#ifdef PLATOONING
-    platooning.update(motor.pwm, motor.steer);
-    if(debug) {
-      platooning.debug(*COM[DEBUG_COM]);
   #ifdef WIFI
-      for(byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
-        if(TCPClient[1][cln]) platooning.debug(TCPClient[1][cln]); 
+    bridge();
   #endif
+
+    deltaMillis = millis() - nextMillisMotorInput;
+    if(deltaMillis >= 0) {
+      nextMillisMotorInput += MOTORINPUT_PERIOD;
+
+  #ifdef PADDELEC
+      paddelec.update(motor.pwm, motor.steer, motor.actualSpeed_kmh, motor.actualSteer_kmh, deltaMillis);
+      if(debug) {
+        paddelec.debug(*COM[DEBUG_COM]);
+        for(byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
+          if(TCPClient[1][cln]) paddelec.debug(TCPClient[1][cln]); 
+      }
+  #endif // PADDELEC
+
+  #ifdef BLE
+  //  loopBLE();
+  motor.pwm = ble_pitch;
+  motor.steer = ble_roll;
+  slowReset(ble_pitch, 0.0, 0.1);
+  slowReset(ble_roll, 0.0, 0.1);
+  #endif
+
+  #ifdef IMU
+  //  imu.loopIMU();
+    imu.update(motor.pwm, motor.steer);
+    imu.debug(*COM[DEBUG_COM]);
+  #endif
+
+  #if defined(NUNCHUCK) && defined(PADDELEC)
+      if(paddelec.gametrak1.r < 500 || paddelec.gametrak2.r < 500) { // switch to nunchuck control when paddle is not used
+  #endif
+  #if defined(NUNCHUCK)
+        int nunchuckError = nunchuk.update(motor.pwm, motor.steer);
+        ++errorCount;
+        nunchuk.debug(*COM[DEBUG_COM]);
+        if(nunchuckError >= 1000) {
+          if(debug) COM[DEBUG_COM]->printf("Reinit Nunchuck %4i ", nunchuckError);
+        } else if(nunchuckError >= 100) {
+          if(debug) COM[DEBUG_COM]->printf("I2C Problems %4i ", nunchuckError);
+        } else if(nunchuckError > 0) {
+          if(debug) COM[DEBUG_COM]->printf("Nunchuck Comm Problems %4i ", nunchuckError);
+        } else  {
+          errorCount = 0;
+        }
+
+        if(errorCount>=5) {
+          nunchuk.reInit();
+          errorCount = 0;
+        }
+  #endif
+  #if defined(NUNCHUCK) && defined(PADDELEC)
+      }
+  #endif
+
+  #ifdef PLATOONING
+      platooning.update(motor.pwm, motor.steer);
+      if(debug) {
+        platooning.debug(*COM[DEBUG_COM]);
+    #ifdef WIFI
+        for(byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
+          if(TCPClient[1][cln]) platooning.debug(TCPClient[1][cln]); 
+    #endif
+      }
+  #endif // PLATOONING
+    } else if(deltaMillis >= MOTORINPUT_PERIOD) { // check if system is too slow
+      if(debug) COM[DEBUG_COM]->print(" Missed Period: ");
+      if(debug) COM[DEBUG_COM]->print(deltaMillis); 
+      if(debug) COM[DEBUG_COM]->print("ms ");
+      
+      nextMillisMotorInput = millis() + MOTORINPUT_PERIOD;
     }
-#endif // PLATOONING
+  }
+}
 
-    /* limit values to a valid range */
-    motor.pwm   = limit(-1000.0, motor.pwm,   1000.0);  
-    motor.steer = limit(-1000.0, motor.steer, 1000.0);  
-
-    int16_t steer = (int16_t) motor.steer;
-    int16_t pwm   = (int16_t) motor.pwm;
+void motorCommunication( void * pvparameters) {
+  int taskno = (int)pvparameters;
+  while(1) {
+    /* cast & limit values to a valid range */
+    int16_t steer = (int16_t) limit(-1000.0, motor.steer, 1000.0);
+    int16_t pwm   = (int16_t) limit(-1000.0, motor.pwm,   1000.0);
     
     /* calc checksum */
     uint32_t crc = 0;
@@ -206,25 +244,20 @@ slowReset(ble_roll, 0.0, 0.1);
       } 
     }
     #endif
-//    if(debug) COM[DEBUG_COM]->print(" U: ");
-//    if(debug) COM[DEBUG_COM]->printf("%6i %6i %11u ", pwm, steer, crc);
-//    if(debug) COM[DEBUG_COM]->printf("%5.2f %5.2f \n", motor.actualSpeed_kmh, motor.actualSteer_kmh);
+    if(debug) COM[DEBUG_COM]->print(" U: ");
+    if(debug) COM[DEBUG_COM]->printf("%6i %6i %11u ", pwm, steer, crc);
+    if(debug) COM[DEBUG_COM]->printf("%5.2f %5.2f \n", motor.actualSpeed_kmh, motor.actualSteer_kmh);
+
+    delay(MOTORINPUT_PERIOD);             
   }
-  if(deltaMillis >= MOTORINPUT_PERIOD) { // check if system is too slow
-    if(debug) COM[DEBUG_COM]->print(" Missed Period: ");
-    if(debug) COM[DEBUG_COM]->print(deltaMillis); 
-    if(debug) COM[DEBUG_COM]->print("ms ");
-    
-    nextMillisMotorInput = millis() + MOTORINPUT_PERIOD;
-  }
-/* TODO
-*  calculate paddle angle. if only gametrak angles are subtracted, the activation threshold depends on the distance (r)
-*  independet variables for left and right wheel, recover MIXER
-*  CRC checksum, maybe message counter?
-*  perfect freewheeling, paddle strenght effect based on paddle angle
-*  define minimum r to function
-*  check phi vor valid values
-*  use timer to get constant deltas
-*  put everything inti functions
-*/
 }
+  /* TODO
+  *  calculate paddle angle. if only gametrak angles are subtracted, the activation threshold depends on the distance (r)
+  *  independet variables for left and right wheel, recover MIXER
+  *  CRC checksum, maybe message counter?
+  *  perfect freewheeling, paddle strenght effect based on paddle angle
+  *  define minimum r to function
+  *  check phi vor valid values
+  *  use timer to get constant deltas
+  *  put everything inti functions
+  */
